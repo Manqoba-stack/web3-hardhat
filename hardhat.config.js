7a09cfe530ab6f20a8e48015d895c79fd55112ac
Manqoba Makhoba <Manyoba1997@gmail.com>
2023-01-07 13:42:04 +0200

    Simple storage project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 
 
@@ -11,7 +13,22 @@ task("accounts", "Prints the list of accounts", async () => {
 });
 
 
+GOERLI_RPC_URL=process.env.GOERLI_RPC_URL
+PRIVATE_KEY=process.env.PRIVATE_KEY
+ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork:"hardhat",
+  networks:{
+    goerli:{
+      url:GOERLI_RPC_URL,
+      accounts:[PRIVATE_KEY],
+      chainId: 5,
+
+    }
+  },
   solidity: "0.8.8",
+  etherscan:{
+    apiKey:ETHERSCAN_API_KEY
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,56 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
- 
+    //grab contract
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    // deploy contract
+    console.log("deploying contract")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+
+    console.log(`Deployed contract to ${simpleStorage.address}`)
+    //verify contract    
+    if(network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY){
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address,[])
+
+    }
+
+    //contract interaction
+    const transactionResponse= await simpleStorage.store(7)
+    await transactionResponse.wait(2)
+
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is ${updatedValue}`)
+
+
+
+    
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            contractArguements: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
